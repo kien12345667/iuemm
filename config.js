8f2ece7a4a7fd8dccb518621dd82f9ccac30aadd
kien12345667 <86776114+kien12345667@users.noreply.github.com>
2021-07-01 17:00:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Thắm béooo!',
+    introDesc: `Lần đầu tiên anh bị sét đánh,
+		Cũng là lần đầu anh gặp emm`,
+    btnIntro: 'iuuuuu',
+    title: 'Bi hỏi em 1 câu quan trọnggggg',
+    desc: 'Em có iu Bi khônggggg ',
+    btnYes: 'Iuu anhhh <33',
+    btnNo: 'Không, có cái nịtt :3',
+    question:'Thế tại sao em lại yêu Biii <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Bởi vì em yêu Bi lắm luôn không có lý doo <33333333',
+    mess: 'Bi biết thừaa 🥰. Bi cũng yêu em nhiều nhiềuuu 😘😘',
+    messDesc: 'Chủ nhật tnay Bi qua với em nhéee.',
+    btnAccept: 'Chốt đơnnn <3',
+    
+}
